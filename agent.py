017d28833dd91773c88cd9eeb2f41bc5f8c0e8b1
tejpshah <55666467+tejpshah@users.noreply.github.com>
2022-12-18 15:46:44 -0500

    group by cluster working for 3x3

--- agent.py
+++ agent.py
@@ -33,10 +33,32 @@ class Agent():
         # forward lookahead to the probability matrix after taking one action 
         p_d, p_u, p_l, p_r = self.move_down(), self.move_up(), self.move_left(), self.move_right()
 
+        # computes the number of clusters that each of the new transition probablities will generate
+        c_d, c_u, c_l, c_r = self.get_num_nonzero_clusters(p_d),  self.get_num_nonzero_clusters(p_u),  self.get_num_nonzero_clusters(p_l),  self.get_num_nonzero_clusters(p_r)
+
         # compute a metric that "scores" the utility of the probability metric
         s_d, s_u, s_l, s_r = self.get_std(p_d), self.get_std(p_u), self.get_std(p_l), self.get_std(p_r)
 
-        utilities = {"D" : s_d, "U" : s_u, "R" : s_r, "L" : s_l}
+        # filters the possible actions that you can take that have the minimal number of non-zero clusters
+        clusters = {"D" : c_d, "U" : c_u, "R" : c_r, "L" : c_l}
+        min_clusters = min(clusters.values())
+
+        # find all possible actions that have the minimal number of clusters
+        possible_actions = list() 
+        for command, cluster in clusters.items():
+            if cluster == min_clusters: possible_actions.append(command)
+
+        # finds the standard deviation of all possible actions with min clusters
+        utilities = dict() 
+        for action in possible_actions:
+            if action == "D": 
+                utilities[action] = s_d 
+            elif action == "L": 
+                utilities[action] = s_l 
+            elif action == "R": 
+                utilities[action] = s_r 
+            elif action == "U": 
+                utilities[action] = s_u 
 
         # greedily select the next action that optimizes the objective function
         max_utility = max(utilities.values())
@@ -66,24 +88,49 @@ class Agent():
         """
         our goal is to move the probability mass to a single point with 100% probability.
         """
-        print(f"\nTHE CURRENT PROBABILITY MATRIX IS: {self.probabilities}")
-
         # forward lookahead to the probability matrix after taking one action 
         p_d, p_u, p_l, p_r = self.move_down(), self.move_up(), self.move_left(), self.move_right()
+
+        # computes the number of clusters that each of the new transition probablities will generate
+        c_d, c_u, c_l, c_r = self.get_num_nonzero_clusters(p_d),  self.get_num_nonzero_clusters(p_u),  self.get_num_nonzero_clusters(p_l),  self.get_num_nonzero_clusters(p_r)
 
         # compute a metric that "scores" the utility of the probability metric
         s_d, s_u, s_l, s_r = self.get_std(p_d), self.get_std(p_u), self.get_std(p_l), self.get_std(p_r)
 
-        print(f"\ns_d = {s_d} and p_d =")
-        print(f"{p_d}")
-        print(f"\ns_u = {s_u} and p_u =")
-        print(f"{p_u}")
-        print(f"\ns_l = {s_l} and p_l =")
-        print(f"{p_l}")
-        print(f"\ns_r = {s_r} and p_r =")
-        print(f"{p_r}")
+        print(f"\nCommand: R \t c_r = {c_r} \t s_r = {s_r}")
+        print(p_r)
 
-        utilities = {"D" : s_d, "U" : s_u, "R" : s_r, "L" : s_l}
+        print(f"\nCommand: L \t c_l = {c_l} \t s_l = {s_l}")
+        print(p_l)
+
+        print(f"\nCommand: U \t c_u = {c_u} \t s_u = {s_u}")
+        print(p_u)
+
+        print(f"\nCommand: D \t c_d = {c_d} \t s_d = {s_d}")
+        print(p_d)
+
+        # filters the possible actions that you can take that have the minimal number of non-zero clusters
+        clusters = {"D" : c_d, "U" : c_u, "R" : c_r, "L" : c_l}
+        min_clusters = min(clusters.values())
+
+        # find all possible actions that have the minimal number of clusters
+        possible_actions = list() 
+        for command, cluster in clusters.items():
+            if cluster == min_clusters: possible_actions.append(command)
+    
+        print(f"\nPossible actions filtered by cluster: {possible_actions}")
+
+        # finds the standard deviation of all possible actions with min clusters
+        utilities = dict() 
+        for action in possible_actions:
+            if action == "D": 
+                utilities[action] = s_d 
+            elif action == "L": 
+                utilities[action] = s_l 
+            elif action == "R": 
+                utilities[action] = s_r 
+            elif action == "U": 
+                utilities[action] = s_u 
 
         # greedily select the next action that optimizes the objective function
         max_utility = max(utilities.values())
@@ -92,12 +139,12 @@ class Agent():
         possible_actions = list() 
         for command, utility in utilities.items():
             if utility == max_utility: possible_actions.append(command)
-
-        print(f"\nThe possible actions are {possible_actions}")
+        
+        print(f"\nPossible actions filtered now by utilities: {possible_actions}")
 
         # randomly select a possible actionto avoid getting stuck in local minima
         action = random.choice(possible_actions)
-        print(f"\nThe action selected is {action}")
+        print(f"\nAction Taken: {action}")
 
         # add the action selected to the trajectory 
         self.actions.append(action)
@@ -111,11 +158,10 @@ class Agent():
             self.probabilities = p_l 
         elif action == "R":
             self.probabilities = p_r 
-
-        # visualize and 2D and 3D probability plots
+        
         self.visualize_nuclear_reactor()
         self.visualize_nuclear_reactor_3d()
-        
+
     def move_deterministically(self, deactivating_path="sequences/sequence-toyexample2.txt"):
         """
         runs the AI agent towards a specified sequence of commands. 
@@ -155,6 +201,59 @@ class Agent():
         """
         return np.std(pmatrix, axis=None, ddof=1) 
 
+    def get_num_nonzero_clusters(self, matrix):
+        """
+        Find the number of connected non-zero clusters in a 2D numpy matrix.
+        
+        Parameters
+        ----------
+        matrix : numpy.ndarray
+            The 2D numpy matrix to search for non-zero clusters.
+        
+        Returns
+        -------
+        int
+            The number of non-zero clusters found in the matrix.
+        
+        Examples
+        --------
+        >>> matrix = np.array([[1, 0, 4], [2, 0, 3], [0, 5, 0]])
+        >>> get_num_nonzero_clusters(matrix)
+        3
+        """
+        # Initialize a visited matrix to track which cells have been visited
+        visited = np.zeros(matrix.shape, dtype=bool)
+        
+        def dfs(i, j):
+            """
+            Depth-first search helper function.
+            """
+            # Mark the current cell as visited
+            visited[i, j] = True
+            
+            # Check the surrounding cells
+            for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                # If the surrounding cell is valid and has a non-zero value, 
+                # and has not been visited yet, recursively search it
+                if (0 <= i + di < matrix.shape[0] and 
+                    0 <= j + dj < matrix.shape[1] and 
+                    matrix[i + di, j + dj] and 
+                    not visited[i + di, j + dj]):
+                    dfs(i + di, j + dj)
+        
+        # Initialize the number of non-zero clusters found
+        num_clusters = 0
+        
+        # Iterate through the matrix and search for non-zero clusters
+        for i in range(matrix.shape[0]):
+            for j in range(matrix.shape[1]):
+                if matrix[i, j] and not visited[i, j]:
+                    # If a non-zero cell has not been visited, it belongs to a new cluster
+                    num_clusters += 1
+                    dfs(i, j)
+        
+        return num_clusters
+      
     # INITIALIZATION FUNCTIONS FOR NUCLEAR REACTOR, PROBABILITIES, & INVALID ACTIONS
 
     def init_nuclear_reactor_config(self):
@@ -367,7 +466,7 @@ if __name__ == "__main__":
     agent = Agent(path="reactors/toyexample2.txt")
     # agent = Agent()
     while not agent.is_terminal_state():
-        agent.move_intelligently()
+        agent.move_intelligently_debug()
     print(f"The optimal action sequence is of length {len(agent.actions)} is {agent.actions}!")
 
 
--- test.py
+++ test.py
@@ -1,7 +1,59 @@
 import numpy as np
 
-arr = np.array([[0.5, 0.2, 1.0], [0.1, 0.7, 0.3]])
-indices = np.where(arr == 1.0)
+import numpy as np
 
-coordinate = (indices[0].item(), indices[1].item())
-print(coordinate)
+def get_num_nonzero_clusters(matrix):
+    """
+    Find the number of connected non-zero clusters in a 2D numpy matrix.
+    
+    Parameters
+    ----------
+    matrix : numpy.ndarray
+        The 2D numpy matrix to search for non-zero clusters.
+    
+    Returns
+    -------
+    int
+        The number of non-zero clusters found in the matrix.
+    
+    Examples
+    --------
+    >>> matrix = np.array([[1, 0, 4], [2, 0, 3], [0, 5, 0]])
+    >>> get_num_nonzero_clusters(matrix)
+    3
+    """
+    # Initialize a visited matrix to track which cells have been visited
+    visited = np.zeros(matrix.shape, dtype=bool)
+    
+    def dfs(i, j):
+        """
+        Depth-first search helper function.
+        """
+        # Mark the current cell as visited
+        visited[i, j] = True
+        
+        # Check the surrounding cells
+        for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            # If the surrounding cell is valid and has a non-zero value, 
+            # and has not been visited yet, recursively search it
+            if (0 <= i + di < matrix.shape[0] and 
+                0 <= j + dj < matrix.shape[1] and 
+                matrix[i + di, j + dj] and 
+                not visited[i + di, j + dj]):
+                dfs(i + di, j + dj)
+    
+    # Initialize the number of non-zero clusters found
+    num_clusters = 0
+    
+    # Iterate through the matrix and search for non-zero clusters
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            if matrix[i, j] and not visited[i, j]:
+                # If a non-zero cell has not been visited, it belongs to a new cluster
+                num_clusters += 1
+                dfs(i, j)
+    
+    return num_clusters
+
+matrix = np.array([[1, 0, 4], [2, 0, 3], [1, 1, 1]])
+print(get_num_nonzero_clusters(matrix))
