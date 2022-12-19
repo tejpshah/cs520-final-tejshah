7e05105acff0d2ca17f4cf2650991d2e38a755f9
tejpshah <55666467+tejpshah@users.noreply.github.com>
2022-12-18 23:25:31 -0500

    update

--- agent.py
+++ agent.py
@@ -1,4 +1,5 @@
 import random 
+import heapq
 import numpy as np 
 import matplotlib.pyplot as plt 
 
@@ -26,233 +27,48 @@ class Agent():
 
     # "INTELLIGENT" LOGIC TO MOVE THE AGENT WITH RESPECT TO THE CORRECT SEQUENCE
     
-    def move_intelligently(self):
-        """
-        our goal is to move the probability mass to a single point with 100% probability.
-        """
-        # forward lookahead to the probability matrix after taking one action 
-        p_d, p_u, p_l, p_r = self.move_down(), self.move_up(), self.move_left(), self.move_right()
+    def a_star(self):
 
-        # computes the number of clusters that each of the new transition probablities will generate
-        c_d, c_u, c_l, c_r = self.get_num_nonzero_clusters(p_d),  self.get_num_nonzero_clusters(p_u),  self.get_num_nonzero_clusters(p_l),  self.get_num_nonzero_clusters(p_r)
+        def cost(sequence):
+            """ @ returns the cost up to the point as number of steps taken"""
+            return len(sequence)
 
-        # compute a metric that "scores" the utility of the probability metric
-        s_d, s_u, s_l, s_r = self.get_std(p_d), self.get_std(p_u), self.get_std(p_l), self.get_std(p_r)
+        def heuristic(probabilities):
+            """ @returns negative log likelihood of the cell with the highest probability """
+            return -np.log(probabilities.max)
 
-        # filters the possible actions that you can take that have the minimal number of non-zero clusters
-        clusters = {"D" : c_d, "U" : c_u, "R" : c_r, "L" : c_l}
-        min_clusters = min(clusters.values())
+        # initialize the heap list and visited set
+        heap, visited = list(), set() 
 
-        # find all possible actions that have the minimal number of clusters
-        possible_actions = list() 
-        for command, cluster in clusters.items():
-            if cluster == min_clusters: possible_actions.append(command)
+        # initialize heap with ( cost(s), ( s, seq(s) ) )
+        init_state, init_seq = self.probabilities, [] 
+        heapq.heappush(heap, (0 + heuristic(init_state), (init_state, init_seq)))
 
-        # finds the standard deviation of all possible actions with min clusters
-        utilities = dict() 
-        for action in possible_actions:
-            if action == "D": 
-                utilities[action] = s_d 
-            elif action == "L": 
-                utilities[action] = s_l 
-            elif action == "R": 
-                utilities[action] = s_r 
-            elif action == "U": 
-                utilities[action] = s_u 
+        # run the A* algorithm until termination 
+        while len(heap) > 0:
 
-        # greedily select the next action that optimizes the objective function
-        max_utility = max(utilities.values())
+            # retrieves the item of minimal cost 
+            _, current_state = heapq.heappop(heap)
+            current_probs, current_seq = current_state
 
-        # find all possible actions that have the maximal utility
-        possible_actions = list() 
-        for command, utility in utilities.items():
-            if utility == max_utility: possible_actions.append(command)
-
-        # randomly select a possible actionto avoid getting stuck in local minima
-        action = random.choice(possible_actions)
-
-        # add the action selected to the trajectory 
-        self.actions.append(action)
-
-        # select an action to take for the probabilities 
-        if action == "D": 
-            self.probabilities = p_d 
-        elif action == "U":
-            self.probabilities = p_u 
-        elif action == "L":
-            self.probabilities = p_l 
-        elif action == "R":
-            self.probabilities = p_r 
-
-    def move_intelligently_debug(self):
-        """
-        our goal is to move the probability mass to a single point with 100% probability.
-        """
-        # forward lookahead to the probability matrix after taking one action 
-        p_d, p_u, p_l, p_r = self.move_down(), self.move_up(), self.move_left(), self.move_right()
-
-        # computes the number of clusters that each of the new transition probablities will generate
-        c_d, c_u, c_l, c_r = self.get_num_nonzero_clusters(p_d),  self.get_num_nonzero_clusters(p_u),  self.get_num_nonzero_clusters(p_l),  self.get_num_nonzero_clusters(p_r)
-
-        # compute a metric that "scores" the utility of the probability metric
-        s_d, s_u, s_l, s_r = self.get_std(p_d), self.get_std(p_u), self.get_std(p_l), self.get_std(p_r)
-
-        print(f"\nCommand: R \t c_r = {c_r} \t s_r = {s_r}")
-        print(p_r)
-
-        print(f"\nCommand: L \t c_l = {c_l} \t s_l = {s_l}")
-        print(p_l)
-
-        print(f"\nCommand: U \t c_u = {c_u} \t s_u = {s_u}")
-        print(p_u)
-
-        print(f"\nCommand: D \t c_d = {c_d} \t s_d = {s_d}")
-        print(p_d)
-
-        # filters the possible actions that you can take that have the minimal number of non-zero clusters
-        clusters = {"D" : c_d, "U" : c_u, "R" : c_r, "L" : c_l}
-        min_clusters = min(clusters.values())
-
-        # find all possible actions that have the minimal number of clusters
-        possible_actions = list() 
-        for command, cluster in clusters.items():
-            if cluster == min_clusters: possible_actions.append(command)
-    
-        print(f"\nPossible actions filtered by cluster: {possible_actions}")
-
-        # finds the standard deviation of all possible actions with min clusters
-        utilities = dict() 
-        for action in possible_actions:
-            if action == "D": 
-                utilities[action] = s_d 
-            elif action == "L": 
-                utilities[action] = s_l 
-            elif action == "R": 
-                utilities[action] = s_r 
-            elif action == "U": 
-                utilities[action] = s_u 
-
-        # greedily select the next action that optimizes the objective function
-        max_utility = max(utilities.values())
-
-        # find all possible actions that have the maximal utility
-        possible_actions = list() 
-        for command, utility in utilities.items():
-            if utility == max_utility: possible_actions.append(command)
-        
-        print(f"\nPossible actions filtered now by utilities: {possible_actions}")
-
-        # randomly select a possible actionto avoid getting stuck in local minima
-        action = random.choice(possible_actions)
-        print(f"\nAction Taken: {action}")
-
-        # add the action selected to the trajectory 
-        self.actions.append(action)
-
-        # select an action to take for the probabilities 
-        if action == "D": 
-            self.probabilities = p_d 
-        elif action == "U":
-            self.probabilities = p_u 
-        elif action == "L":
-            self.probabilities = p_l 
-        elif action == "R":
-            self.probabilities = p_r 
-        
-        self.visualize_nuclear_reactor()
-        self.visualize_nuclear_reactor_3d()
-
-    def move_deterministically(self, deactivating_path="sequences/sequence-toyexample2.txt"):
-        """
-        runs the AI agent towards a specified sequence of commands. 
-        """
-
-        # loads in a sequence of actions to localize the robot 
-        self.deactivating_path = deactivating_path
-        command_sequence = self.load_deactivating_sequence()
-
-        print(command_sequence)
-
-        # runs a sequence of commands 
-        for command in command_sequence:
-
-            print(f"MOVING {command}!")
-
-            # update probabilities according to command 
-            if command == "L": 
-                self.probabilities = self.move_left()
-            elif command == "R": 
-                self.probabilities = self.move_right()
-            elif command == "U": 
-                self.probabilities = self.move_up()
-            elif command == "D": 
-                self.probabilities = self.move_down()
-
-            # visualize and 2D and 3D probability plots
-            self.visualize_nuclear_reactor()
-            self.visualize_nuclear_reactor_3d()
-        
-        # print out the location of localized coordinate
-        self.get_localized_robot_location()
-        
-    def get_std(self, pmatrix):
-        """
-        returns the sample standard deviation of a matrix to determine which location to move to
-        """
-        return np.std(pmatrix, axis=None, ddof=1) 
-
-    def get_num_nonzero_clusters(self, matrix):
-        """
-        Find the number of connected non-zero clusters in a 2D numpy matrix.
-        
-        Parameters
-        ----------
-        matrix : numpy.ndarray
-            The 2D numpy matrix to search for non-zero clusters.
-        
-        Returns
-        -------
-        int
-            The number of non-zero clusters found in the matrix.
-        
-        Examples
-        --------
-        >>> matrix = np.array([[1, 0, 4], [2, 0, 3], [0, 5, 0]])
-        >>> get_num_nonzero_clusters(matrix)
-        3
-        """
-        # Initialize a visited matrix to track which cells have been visited
-        visited = np.zeros(matrix.shape, dtype=bool)
-        
-        def dfs(i, j):
-            """
-            Depth-first search helper function.
-            """
-            # Mark the current cell as visited
-            visited[i, j] = True
+            # returns the sequence of moves if terminal state
+            if self.is_terminal_state(current_probs):
+                return current_seq 
             
-            # Check the surrounding cells
-            for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                # If the surrounding cell is valid and has a non-zero value, 
-                # and has not been visited yet, recursively search it
-                if (0 <= i + di < matrix.shape[0] and 
-                    0 <= j + dj < matrix.shape[1] and 
-                    matrix[i + di, j + dj] and 
-                    not visited[i + di, j + dj]):
-                    dfs(i + di, j + dj)
-        
-        # Initialize the number of non-zero clusters found
-        num_clusters = 0
-        
-        # Iterate through the matrix and search for non-zero clusters
-        for i in range(matrix.shape[0]):
-            for j in range(matrix.shape[1]):
-                if matrix[i, j] and not visited[i, j]:
-                    # If a non-zero cell has not been visited, it belongs to a new cluster
-                    num_clusters += 1
-                    dfs(i, j)
-        
-        return num_clusters
+            # if we've already visited this state before continue 
+            if tuple(current_probs.flatten()) in visited: 
+                continue 
+            
+            # mark the current state as visited 
+            visited.add(tuple(current_probs.flatten()))
+
+            # iterate through every possible action possible 
+            for action in ["U", "D", "L", "R"]:
+                
+
+            
+
+
       
     # INITIALIZATION FUNCTIONS FOR NUCLEAR REACTOR, PROBABILITIES, & INVALID ACTIONS
 
@@ -364,12 +180,12 @@ class Agent():
                 else: p_left[i, j] += self.probabilities[i,j]
         return p_left 
 
-    def is_terminal_state(self):
+    def is_terminal_state(self, probabilities):
         """
         ends the game if 1.0 is in any of the probabilities (i.e. we are 100% confident on localizing the drone)
         """ 
-        for i in range(0, self.probabilities.shape[0]):
-            for j in range(0, self.probabilities.shape[1]):
+        for i in range(0, probabilities.shape[0]):
+            for j in range(0, probabilities.shape[1]):
                 if self.probabilities[i,j] > 0.999: 
                     return True 
         return False 
